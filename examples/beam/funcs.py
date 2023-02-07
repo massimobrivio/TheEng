f83d26fba590010e39da94c9b2182415ea06cd9f
Massimo Brivio <brivio@radiate.ch>
2023-02-07 17:44:04 +0100

    reorganize file structure

--- examples/beam/funcs.py
+++
@@ -1,3 +0,0 @@
-def objective(x):
-    out = dict(f1=(4 * 10000.0 / 193140) * x[0] ** 3 / (x[1] * x[2] ** 3))
-    return out
--- examples/multiobjective_optsampling/funcs.py
+++
@@ -1,3 +0,0 @@
-def objective(x, *args):
-    out = dict(f1=100 * (x[0] ** 2 + x[1] ** 2), f2=1e6 * (x[0] - 1) ** 2 + x[1] ** 2)
-    return out
