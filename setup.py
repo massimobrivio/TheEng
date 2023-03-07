e7f18f4f6e84785384513ee3e52762760e079a0d
Massimo Brivio <brivio@radiate.ch>
2023-03-07 16:03:52 +0100

    refactoring for install

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author="Massimo Brivio",
     author_email="m.g.brivio@gmail.com",
     description=DESCRIPTION,
-    packages=find_packages(where=".", include=["core/*"]),
+    packages=find_packages(where=".", include=["theeng*"]),
     long_description_content_type="text/markdown",
     long_description="TheEng is a Python library for dealing with structural optimization.",
     install_requires=required,
@@ -24,3 +24,4 @@ setup(
 # 1 python setup.py sdist bdist
 # 2 twine upload --repository testpypi dist/*
 # 3 twine upload dist/* --> only for final release
+# print(find_packages(where="src"))
